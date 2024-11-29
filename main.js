3703779bfdbeacf3e70f8973bc437bf387f6455f
Federico Nova <fedenova8@gmail.com>
2024-11-29 14:03:35 -0300

    Creamos la primer parte del desafío

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 import './style.css'
+
+//! ------------------------------------------ PARTE 1 -------------------------------------------------- 
+
+console.warn('PARTE 1');
+
+let nombre = 'Fede'; 
+let apellido = 'Nova';
+let edad = 25; 
+let pais = 'Uruguay';
+
+let conocimientos = ['Inglés', 'Español', 'Cocinar'];
+
+console.log(nombre);
+console.log(apellido);
+console.log(edad);
+console.log(pais);
+
+console.log(conocimientos);
+
+edad = 26;
+console.log(edad);
+
+let nuevaLongitudConocimientos = conocimientos.push('Conducir');
+console.log(conocimientos);
